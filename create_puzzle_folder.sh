f0596de653a0517efec769617e6589dd20602851
abeldewit <abeldewit@gmail.com>
2023-12-04 17:49:49 +0100

    Updated bash script for new decorator style

--- create_puzzle_folder.sh
+++ create_puzzle_folder.sh
@@ -1,14 +1,8 @@
 read -p "Year: " YEAR
 read -p "Puzzle num : " NUM
 
-echo "$YEAR $NUM"
-mkdir -p "$YEAR/$NUM"
+mkdir -p "advent_of_code/$YEAR/$NUM"
 
-echo "from pathlib import Path\npuzzle_input = Path(__file__).parent.joinpath('puzzle_input.txt')\nwith open(puzzle_input, 'r') as f:\n    lines = [line.strip() for line in f.readlines()]" >> "$YEAR/$NUM/$NUM.py"
+cp "solution_template.py" "advent_of_code/$YEAR/$NUM/$NUM.py"
+echo "" >> "advent_of_code/$YEAR/$NUM/puzzle_input.txt"
 
-echo "Enter the puzzle input :"
-while read -r domain
-do
-
-    echo "$domain" >> "$YEAR/$NUM/puzzle_input.txt"
-done

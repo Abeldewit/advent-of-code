8498455d12f0532a3eb2286ab4e2987e66eda21b
Abel de Wit <abel.de.wit@nl.abnamro.com>
2023-12-05 13:38:13 +0100

    08/2022

--- advent_of_code/2022/08/08.py
+++ advent_of_code/2022/08/08.py
@@ -1,66 +1,82 @@
+from advent_of_code.decorators import aoc_output
+from advent_of_code.utilities import get_puzzle_input
+
 from pathlib import Path
 import numpy as np
-
-with open(Path(__file__).parent.joinpath('puzzle_input.txt'), 'r') as f:
-    lines = f.readlines()
-
-boom_grid = np.array([list(l.strip()) for l in lines]).astype(int)
-skirt_count = np.sum((boom_grid.shape, boom_grid.shape)) - 4
+from typing import List
 
 
-def is_visible(grid, x, y):
-    self_height = grid[x, y]
+@aoc_output(title="Day 8 - Treetop Tree House")
+def get_visible_trees(lines: List[str]) -> int:
+    boom_grid = np.array([list(l.strip()) for l in lines]).astype(int)
+    skirt_count = np.sum((boom_grid.shape, boom_grid.shape)) - 4
 
-    horizontal = grid[x, :]
-    vertical = grid[:, y]
-    
-    left = horizontal[:y]
-    right = horizontal[y+1:]
-    top = vertical[:x]
-    bottom = vertical[x+1:]
+    def is_visible(grid, x, y):
+        self_height = grid[x, y]
 
-    vis_top = len(np.where(top >= self_height)[0]) == 0
-    vis_bottom = len(np.where(bottom >= self_height)[0]) == 0
-    vis_left = len(np.where(left >= self_height)[0]) == 0
-    vis_right = len(np.where(right >= self_height)[0]) == 0
-
-    visible = any((vis_left, vis_right, vis_top, vis_bottom))
-    if visible:
-        return 1
-    return 0
-
-
-def scenic_score(grid, x, y):
-    self_height = grid[x, y]
-
-    horizontal = grid[x, :]
-    vertical = grid[:, y]
-
-    left = horizontal[:y]
-    right = horizontal[y+1:]
-    top = vertical[:x]
-    bottom = vertical[x+1:]
-
-    scores = []
-    for direction in (left[::-1], right, top[::-1], bottom):
-        dir_score = 0
-        for tree in direction:
-            if tree < self_height:
-                dir_score += 1
-                continue
-            break
-        if dir_score == 0:
-            dir_score = 1
-        scores.append(dir_score)
-    print(f"x:{x}, y:{y}, score: {np.product(scores)}")
-    return np.product(scores)
-
-
-visible_count = 0
-for y in range(1, boom_grid.shape[1]-1):
-    for x in range(1, boom_grid.shape[0]-1):
-        visible_count += is_visible(boom_grid, y, x)
-        scenic_score(boom_grid, y, x)
+        horizontal = grid[x, :]
+        vertical = grid[:, y]
         
-print(f"Edges: {skirt_count}, High trees: {visible_count}\tTotal: {skirt_count+visible_count}")
+        left = horizontal[:y]
+        right = horizontal[y+1:]
+        top = vertical[:x]
+        bottom = vertical[x+1:]
 
+        vis_top = len(np.where(top >= self_height)[0]) == 0
+        vis_bottom = len(np.where(bottom >= self_height)[0]) == 0
+        vis_left = len(np.where(left >= self_height)[0]) == 0
+        vis_right = len(np.where(right >= self_height)[0]) == 0
+
+        visible = any((vis_left, vis_right, vis_top, vis_bottom))
+        if visible:
+            return 1
+        return 0
+
+    visible_count = 0
+    for y in range(1, boom_grid.shape[1]-1):
+        for x in range(1, boom_grid.shape[0]-1):
+            visible_count += is_visible(boom_grid, y, x)
+    return visible_count
+
+
+@aoc_output(title='Day 8 - Scenic scores')
+def get_scenic_score(lines: List[str]) -> int:
+    def _scenic_score(grid, x, y):
+        self_height = grid[x, y]
+
+        horizontal = grid[x, :]
+        vertical = grid[:, y]
+
+        left = horizontal[:y]
+        right = horizontal[y+1:]
+        top = vertical[:x]
+        bottom = vertical[x+1:]
+
+        scores = []
+        for direction in (top[::-1], left[::-1], bottom, right):
+            # if edge, make zero
+            if len(direction) == 0:
+                return 0
+            
+            dir_score = 0
+            for tree in direction:
+                dir_score += 1
+                if tree >= self_height:
+                    break
+            if dir_score == 0:
+                dir_score = 1
+            scores.append(dir_score)
+        return np.prod(scores)
+        
+    boom_grid = np.array([list(l.strip()) for l in lines]).astype(int)
+    scenic_scores = np.zeros(boom_grid.shape)
+    for x in range(len(lines[0])):
+        for y in range(len(lines)):
+            scenic_scores[x, y] = _scenic_score(boom_grid, x, y)
+    return int(np.max(scenic_scores))
+
+
+if __name__ == "__main__":
+    pi = get_puzzle_input(__file__)
+    get_visible_trees(pi)
+    get_scenic_score(pi)

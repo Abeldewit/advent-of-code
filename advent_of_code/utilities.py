bf401b4e0e8bd1e488abc5f5a5566fb7c3db319c
abeldewit <abeldewit@gmail.com>
2023-12-05 22:09:01 +0100

    Added single line option to get_puzzle_input

--- advent_of_code/utilities.py
+++ advent_of_code/utilities.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 
-def get_puzzle_input(python_file_path: str):
+def get_puzzle_input(python_file_path: str, single_line: bool = False):
     puzzle_input = Path(python_file_path).parent.joinpath('puzzle_input.txt')
     with open(puzzle_input, 'r') as f:
+        if single_line:
+            return f.read()
         return [line.strip() for line in f.readlines()]

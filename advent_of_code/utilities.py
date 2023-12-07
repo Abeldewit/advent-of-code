412f22b3812e362512bac2aa63f77c0364e48681
abeldewit <abeldewit@gmail.com>
2023-12-07 08:37:00 +0100

    utils style changes

--- advent_of_code/utilities.py
+++ advent_of_code/utilities.py
@@ -7,7 +7,8 @@ def get_puzzle_input_from_file(python_file_path: str):
     puzzle_input = Path(python_file_path).parent.joinpath('puzzle_input.txt')
     with open(puzzle_input, 'r') as f:
         return f.read()
-    
+
+
 def get_puzzle_input(
     python_file_path: str,
     split_lines: bool = True,
@@ -16,7 +17,7 @@ def get_puzzle_input(
 ):
     puzzle_folder = Path(python_file_path).parent
     year, day = map(int, str(puzzle_folder).split('/')[-2:])
-    
+
     if puzzle_folder.joinpath('puzzle_input.txt').exists():
         puzzle_data = get_puzzle_input_from_file(python_file_path)
     else:
@@ -26,17 +27,10 @@ def get_puzzle_input(
             )
         except PuzzleLockedError as ple:
             return str(ple)
-    
     if save_to_file:
         with open(puzzle_folder.joinpath('puzzle_input.txt'), 'w') as puzzle_file:
             puzzle_file.write(puzzle_data)
-    
+
     if split_lines:
         return [line.strip() for line in puzzle_data.split('\n')]
     return puzzle_data
-    
-    
-
-
-if __name__ == "__main__":
-    get_puzzle_input_auto('2023', '06')

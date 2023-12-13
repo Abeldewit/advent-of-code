0639f8c7919f4bf329acda3e8f29a3c34a390f9a
abeldewit <abeldewit@gmail.com>
2023-12-13 09:24:20 +0100

    13 cleanup duplicate code

--- advent_of_code/2023/13/13.py
+++ advent_of_code/2023/13/13.py
@@ -1,76 +1,43 @@
 from advent_of_code.decorators import aoc_output
-from advent_of_code.utilities import get_puzzle_input, submit_solution
-
+from advent_of_code.utilities import get_puzzle_input
 from typing import List
+
+
+def find_mirror_line(m, diff_allowance: int = 0) -> int:
+    mirror_lines = 0
+    for i in range(len(m)):
+        diff_count = 0
+        for row_1, row_2 in zip(m[i-1::-1], m[i:]):
+            for col_1, col_2 in zip(row_1, row_2):
+                if col_1 != col_2:
+                    diff_count += 1
+        if diff_count == diff_allowance:
+            mirror_lines += 100 * i
+            break
+
+    rot_m = [*zip(*m)]
+    for j in range(len(rot_m)):
+        diff_count = 0
+        for row_1, row_2 in zip(rot_m[j-1::-1], rot_m[j:]):
+            for col_1, col_2 in zip(row_1, row_2):
+                if col_1 != col_2:
+                    diff_count += 1
+        if diff_count == diff_allowance:
+            mirror_lines += j
+            break
+    return mirror_lines
 
 
 @aoc_output(title="Day 13 - Mirrors")
 def part_1(lines: List[str]) -> int:
     maps = list(map(str.splitlines, lines.split('\n\n')))
-
-    total_count = 0
-    for m in maps:
-        vertical, horizontal = 0, 0
-        for i in range(len(m)):
-            diff_count = 0
-            for row_1, row_2 in zip(m[i-1::-1], m[i:]):
-                for col_1, col_2 in zip(row_1, row_2):
-                    if col_1 != col_2:
-                        diff_count += 1
-            if diff_count == 0:
-                vertical = i
-                break
-        if vertical != 0:
-            total_count += 100 * vertical
-            continue
-
-        rot_m = [*zip(*m)]
-        for j in range(len(rot_m)):
-            diff_count = 0
-            for row_1, row_2 in zip(rot_m[j-1::-1], rot_m[j:]):
-                for col_1, col_2 in zip(row_1, row_2):
-                    if col_1 != col_2:
-                        diff_count += 1
-            if diff_count == 0:
-                horizontal = j
-                break
-        total_count += horizontal
-
-    return total_count
+    return sum(find_mirror_line(m) for m in maps)
 
 
 @aoc_output(title="Day 13 - Smudged Mirrors")
 def part_2(lines: List[str]) -> int:
     maps = list(map(str.splitlines, lines.split('\n\n')))
-
-    total_count = 0
-    for m in maps:
-        vertical, horizontal = 0, 0
-        for i in range(len(m)):
-            diff_count = 0
-            for row_1, row_2 in zip(m[i-1::-1], m[i:]):
-                for col_1, col_2 in zip(row_1, row_2):
-                    if col_1 != col_2:
-                        diff_count += 1
-            if diff_count == 1:
-                vertical = i
-                break
-        if vertical != 0:
-            total_count += 100 * vertical
-            continue
-
-        rot_m = [*zip(*m)]
-        for j in range(len(rot_m)):
-            diff_count = 0
-            for row_1, row_2 in zip(rot_m[j-1::-1], rot_m[j:]):
-                for col_1, col_2 in zip(row_1, row_2):
-                    if col_1 != col_2:
-                        diff_count += 1
-            if diff_count == 1:
-                horizontal = j
-                break
-        total_count += horizontal
-    return total_count
+    return sum(find_mirror_line(m, 1) for m in maps)
 
 
 if __name__ == "__main__":
@@ -78,8 +45,6 @@ if __name__ == "__main__":
 
     # Part 1
     solution_1 = part_1(pi)
-    submit_solution(__file__, solution=solution_1, part='a')
 
     # Part 2
     solution_2 = part_2(pi)
-    submit_solution(__file__, solution=solution_2, part='b')

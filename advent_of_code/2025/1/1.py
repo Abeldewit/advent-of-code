aef00008119627a936f587bbe635e112a06cdd30
Abel de Wit <abeldewit@gmail.com>
2025-12-01 17:20:19 +0100

    Adding comments to day 1 and running linters for the template

--- advent_of_code/2025/1/1.py
+++ advent_of_code/2025/1/1.py
@@ -1,27 +1,48 @@
 from typing import List
 
+from aocd.models import Puzzle
+
 from advent_of_code.decorators import aoc_output
-from advent_of_code.utilities import get_puzzle_input, submit_solution
+from advent_of_code.utilities import (convert_to_numbers,
+                                      create_array_from_lines,
+                                      get_puzzle_input, get_puzzle_lines,
+                                      submit_solution)
+
+PUZZLE = Puzzle(year=2025, day=1)
 
 
-@aoc_output(title="Day - ")
+@aoc_output(title=f"Day 1/2025/a - {PUZZLE.title}")
 def part_1(lines: List[str]) -> int:
+    """Turn a dial with numbers 0-99 and count whenever its at zero.
+
+    Using the modulo operator (which also works with negatives) we can
+    calculate where the dial will stop with simple subtraction/addition
+    based on turning left or right.
+
+    Args:
+        lines: A list of lines containing the direction and number of clicks.
+
+    Returns:
+        The total amount of stops at zero.
+    """
     dial_num = 50
     zero_counter = 0
     for line in lines:
         if not line:
             continue
+        print(line)
         turn = int(line[1:])
         if line[0] == "L":
             turn *= -1
         dial_num = (dial_num + turn) % 100
 
+        # Count whenever the dial stops at exactly zero
         if dial_num == 0:
             zero_counter += 1
     return zero_counter
 
 
-@aoc_output(title="Day - ")
+@aoc_output(title=f"Day 1/2025/b - {PUZZLE.title}")
 def part_2(lines: List[str]) -> int:
     dial_num = 50
     zero_counter = 0
@@ -33,6 +54,7 @@ def part_2(lines: List[str]) -> int:
         else:
             left = False
 
+        # Compared to part 1, we need to do every 'click' and count whenever it passes zero
         for _ in range(int(line[1:])):
             dial_num = (dial_num + (1 if not left else -1)) % 100
             if dial_num == 0:
@@ -41,7 +63,7 @@ def part_2(lines: List[str]) -> int:
 
 
 if __name__ == "__main__":
-    pi = get_puzzle_input(__file__, block=True)
+    pi = get_puzzle_lines(__file__, block=True)
 
     # Part 1
     solution_1 = part_1(pi)
--- solution_template.py
+++ solution_template.py
@@ -1,16 +1,18 @@
+from typing import List
+
+from aocd.models import Puzzle
+
 from advent_of_code.decorators import aoc_output
 from advent_of_code.utilities import (
+    convert_to_numbers,
+    create_array_from_lines,
     get_puzzle_input,
     get_puzzle_lines,
     submit_solution,
-    convert_to_numbers,
-    create_array_from_lines,
 )
-from aocd.models import Puzzle
-from typing import List
-
 
 PUZZLE = Puzzle(year={YEAR}, day={DAY})
+
 
 @aoc_output(title=f"Day {DAY}/{YEAR}/a - {PUZZLE.title}")
 def part_1(lines: List[str]) -> int | None:
